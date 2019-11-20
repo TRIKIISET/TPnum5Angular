d7b41104c1ec8bd4f097a922659bbf81784f65e0
AMTRIKI <amel.triki@gmail.com>
2019-11-21 00:25:42 +0100

    tp A envoyer aux étudiants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,13 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { LivreComponent } from './ex2/livre/livre.component';
-import { ListLivresComponent } from './ex2/list-livres/list-livres.component';
+
 import { PrincipalComponent } from './application/principal/principal.component';
 import { ListEmployesComponent } from './ex1/list-employes/list-employes.component';
 import { AddEmployeComponent } from './ex1/add-employe/add-employe.component';
 import { FirstComponent } from './application/first/first.component';
 import { SecondComponent } from './application/second/second.component';
-import { ExempleComponent } from './essai/exemple/exemple.component';
-import { EtudiantComponent } from './essai/etudiant/etudiant.component';
+
 
 
 
@@ -20,15 +18,11 @@ import { EtudiantComponent } from './essai/etudiant/etudiant.component';
 @NgModule({
   declarations: [
     AppComponent,
-    LivreComponent,
-    ListLivresComponent,
     PrincipalComponent,
     ListEmployesComponent,
     AddEmployeComponent,
     FirstComponent,
     SecondComponent,
-    ExempleComponent,
-    EtudiantComponent  
   ],
   imports: [
     BrowserModule,
